c313be7de7f0db3bf63a48047f69b177f7587a76
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-19 18:16:23 +0900

    20200319-1

--- implicit-server/src/main/java/com/example/implicitserver/config/OAuth2AuthorizationServer.java
+++ implicit-server/src/main/java/com/example/implicitserver/config/OAuth2AuthorizationServer.java
@@ -15,7 +15,8 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
                 .withClient("clientapp")
                 .secret("123456")
                 .redirectUris("http://localhost:9000/callback")
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("implicit")
+                .accessTokenValiditySeconds(120)
                 .scopes("read_profile", "read_contacts");
     }
 
