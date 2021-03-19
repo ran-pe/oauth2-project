82991cf0947ad6bc7926b15e879eeaa68ec441fd
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-19 17:57:54 +0900

    20200319

---
+++ implicit-server/src/main/java/com/example/implicitserver/api/UserProfile.java
@@ -0,0 +1,12 @@
+package com.example.implicitserver.api;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserProfile {
+    private String name;
+    private String email;
+}
---
+++ implicit-server/src/test/java/com/example/implicitserver/ImplicitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implicitserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplicitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
