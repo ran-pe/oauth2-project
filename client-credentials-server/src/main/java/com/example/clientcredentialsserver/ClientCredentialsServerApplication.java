a1c54d877fbc44564efc8e0b37ff5315ee63de16
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-23 12:02:05 +0900

    20200323-clientCredentialsServer

---
+++ client-credentials-server/src/main/java/com/example/clientcredentialsserver/ClientCredentialsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientcredentialsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCredentialsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientCredentialsServerApplication.class, args);
+    }
+
+}
