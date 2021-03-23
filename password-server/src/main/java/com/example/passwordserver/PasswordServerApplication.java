0a8ea37cf22d2ab816542ce0c03ab83b2cbfd953
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-23 09:03:48 +0900

    20200323

---
+++ password-server/src/main/java/com/example/passwordserver/PasswordServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.passwordserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordServerApplication.class, args);
+    }
+
+}
---
+++ password-server/src/test/java/com/example/passwordserver/PasswordServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passwordserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
