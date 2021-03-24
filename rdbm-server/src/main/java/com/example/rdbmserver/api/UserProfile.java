290518ba4e86cdbc1f7c6fed05c8692815c1aa6d
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-24 10:02:21 +0900

    20200324-rdbmServer add

---
+++ rdbm-server/src/main/java/com/example/rdbmserver/api/UserProfile.java
@@ -0,0 +1,12 @@
+package com.example.rdbmserver.api;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserProfile {
+    private String name;
+    private String email;
+}
--- rdbm-server/src/test/java/com/example/rdbmserver/RdbmServerApplicationTests.java
+++ rdbm-server/src/test/java/com/example/rdbmserver/RdbmServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.rdbmserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
