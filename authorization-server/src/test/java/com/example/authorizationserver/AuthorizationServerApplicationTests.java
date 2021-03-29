186d6ddbb2544283b39bfbdb709611771c4b81ee
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-29 13:40:11 +0900

    20200329-authoriaztionServer&resourceServer add

---
+++ authorization-server/src/test/java/com/example/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorizationserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ resource-server/src/test/java/com/example/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
