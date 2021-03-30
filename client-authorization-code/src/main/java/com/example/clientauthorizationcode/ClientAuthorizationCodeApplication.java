998307b259a4e9ea495d085abdd40c8fc0148849
youngran.kwon <youngran.kwon@lemonhc.com>
2021-03-30 09:26:13 +0900

    20200330-clientAuthorizationCode add

---
+++ client-authorization-code/src/main/java/com/example/clientauthorizationcode/ClientAuthorizationCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientauthorizationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthorizationCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAuthorizationCodeApplication.class, args);
+    }
+
+}
---
+++ client-authorization-code/src/test/java/com/example/clientauthorizationcode/ClientAuthorizationCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientauthorizationcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAuthorizationCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
