de9de8bca560a1701b81458dc3cb1c70b7cbfd56
youngran.kwon <youngran.kwon@lemonhc.com>
2021-04-08 14:54:13 +0900

    20200408-resource-server-jwt add

---
+++ resource-server-jwt/src/main/java/com/example/resourceserverjwt/ResourceServerJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourceserverjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerJwtApplication.class, args);
+    }
+
+}
