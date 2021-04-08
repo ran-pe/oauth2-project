eee507c57604b458e1bc1c7273cda8c723662ac5
youngran.kwon <youngran.kwon@lemonhc.com>
2021-04-08 16:30:47 +0900

    20200408-resource-server-jwt add

--- resource-server-jwt/src/main/java/com/example/resourceserverjwt/api/UserController.java
+++ resource-server-jwt/src/main/java/com/example/resourceserverjwt/api/UserController.java
@@ -24,5 +24,21 @@ public class UserController {
             this.name = name;
             this.email = email;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 }
