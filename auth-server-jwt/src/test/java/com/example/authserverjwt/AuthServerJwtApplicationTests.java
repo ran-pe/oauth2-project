317924d92b4176cb9e11aa24977a75c3bc611f0f
youngran.kwon <youngran.kwon@lemonhc.com>
2021-04-08 12:13:29 +0900

    20200408-auth-server-jwt add

---
+++ auth-server-jwt/src/test/java/com/example/authserverjwt/AuthServerJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authserverjwt;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
